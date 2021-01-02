356a1e62b7fa53bf56278c22a6bf51bc56a5964e
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-01-03 06:27:23 +0900

    Use double linked list

--- libs/graphics/src/main/kotlin/mono/list/QuickList.kt
+++ libs/graphics/src/main/kotlin/mono/list/QuickList.kt
@@ -7,37 +7,33 @@ package mono.list
  * the head or the tail or after an specific item.
  */
 class QuickList<T : QuickList.Identifier> : Collection<T> {
-    // TODO: Use double linked list for list to make add(), remove(), move() O(1).
-    private val list: MutableList<T> = mutableListOf()
-    private val map: MutableMap<Int, T> = mutableMapOf()
+    private val linkedList: DoubleLinkedList<T> = DoubleLinkedList()
+    private val map: MutableMap<Int, Node<T>> = mutableMapOf()
 
     override val size: Int
-        get() = list.size
+        get() = map.size
 
     override fun contains(element: T): Boolean = element.id in map
 
     override fun containsAll(elements: Collection<T>): Boolean = elements.all { it in this }
 
-    override fun isEmpty(): Boolean = list.isEmpty()
+    override fun isEmpty(): Boolean = map.isEmpty()
 
-    override fun iterator(): Iterator<T> = list.iterator()
+    override fun iterator(): Iterator<T> = linkedList.iterator()
 
     fun add(element: T, position: AddPosition = AddPosition.Last): Boolean {
         if (element in this) {
             return false
         }
-        map[element.id] = element
 
-        val index = when (position) {
-            AddPosition.Last -> size
-            AddPosition.First -> 0
-            is AddPosition.After -> {
-                val previous = map[position.identifier.id]
-                val previousIndex = list.indexOf(previous)
-                if (previousIndex >= 0) previousIndex + 1 else size
-            }
-        }
-        list.add(index, element)
+        val preNode = when (position) {
+            AddPosition.Last -> linkedList.tail.pre
+            AddPosition.First -> linkedList.head
+            is AddPosition.After -> map[position.identifier.id]
+        } ?: return false
+        val node = linkedList.add(element, preNode)
+        map[element.id] = node
+
         return true
     }
 
@@ -54,69 +50,28 @@ class QuickList<T : QuickList.Identifier> : Collection<T> {
     }
 
     fun remove(identifier: Identifier): T? {
-        if (identifier.id !in map) {
-            return null
-        }
-        val element = map.remove(identifier.id)
-        list.remove(element)
-        return element
+        val node = map.remove(identifier.id) ?: return null
+        linkedList.remove(node)
+
+        return node.value
     }
 
     fun removeAll(): List<T> {
-        val result = list.toList()
-        list.clear()
+        val result = linkedList.iterator().asSequence().toList()
+        linkedList.clear()
         map.clear()
         return result
     }
 
-    operator fun get(id: Int): T? = map[id]
+    operator fun get(id: Int): T? = map[id]?.value
 
     fun move(identifier: Identifier, moveActionType: MoveActionType) {
         if (size < 2) {
             return
         }
-        when (moveActionType) {
-            MoveActionType.UP -> moveUp(identifier)
-            MoveActionType.DOWN -> moveDown(identifier)
-            MoveActionType.TOP -> moveTop(identifier)
-            MoveActionType.BOTTOM -> moveBottom(identifier)
-        }
-    }
 
-    private fun moveUp(identifier: Identifier) {
-        if (list.last().id == identifier.id) {
-            return
-        }
-        val shape = map[identifier.id] ?: return
-        val index = list.indexOf(shape)
-        list[index] = list[index + 1]
-        list[index + 1] = shape
-    }
-
-    private fun moveDown(identifier: Identifier) {
-        if (list.first().id == identifier.id) {
-            return
-        }
-        val shape = map[identifier.id] ?: return
-        val index = list.indexOf(shape)
-        list[index] = list[index - 1]
-        list[index - 1] = shape
-    }
-
-    private fun moveTop(identifier: Identifier) {
-        if (list.last().id == identifier.id) {
-            return
-        }
-        val shape = remove(identifier) ?: return
-        add(shape)
-    }
-
-    private fun moveBottom(identifier: Identifier) {
-        if (list.first().id == identifier.id) {
-            return
-        }
-        val shape = remove(identifier) ?: return
-        add(shape, AddPosition.First)
+        val node = map[identifier.id] ?: return
+        linkedList.move(node, moveActionType)
     }
 
     interface Identifier {
@@ -131,5 +86,76 @@ class QuickList<T : QuickList.Identifier> : Collection<T> {
 
     enum class MoveActionType {
         UP, DOWN, TOP, BOTTOM
+    }
+
+    private class Node<T>(var pre: Node<T>?, var next: Node<T>?, val value: T?)
+
+    private class DoubleLinkedList<T> {
+        // Publishing head and tail is unsafe. However, since they are only used under QuickList
+        // context, this is acceptable. head and tail's next and pre pointers must not be changed
+        // outside this class.
+        val head: Node<T> = Node(null, null, null)
+        val tail: Node<T> = Node(null, null, null)
+
+        init {
+            head.next = tail
+            tail.pre = head
+        }
+
+        fun add(value: T, previousNode: Node<T>): Node<T> {
+            val afterNode = previousNode.next
+            val node = Node(previousNode, afterNode, value)
+            previousNode.next = node
+            afterNode?.pre = node
+            return node
+        }
+
+        fun remove(node: Node<T>) {
+            val previousNode = node.pre
+            val afterNode = node.next
+            previousNode?.next = afterNode
+            afterNode?.pre = previousNode
+            node.next = null
+            node.pre = null
+        }
+
+        fun clear() {
+            head.next?.pre = null
+            tail.pre?.next = null
+            head.next = tail
+            tail.pre = head
+        }
+
+        fun move(node: Node<T>, moveActionType: MoveActionType) {
+            val newPreviousNode = when (moveActionType) {
+                MoveActionType.UP -> node.next
+                MoveActionType.DOWN -> node.pre?.pre
+                MoveActionType.TOP -> tail.pre
+                MoveActionType.BOTTOM -> head
+            } ?: return
+            remove(node)
+
+            val newNextNode = newPreviousNode.next
+            newPreviousNode.next = node
+            newNextNode?.pre = node
+            node.pre = newPreviousNode
+            node.next = newNextNode
+        }
+
+        fun iterator(): Iterator<T> = DoubleLinkedListIterator(head)
+
+        private class DoubleLinkedListIterator<T>(private var head: Node<T>?) : Iterator<T> {
+
+            override fun hasNext(): Boolean {
+                val nextNext = head?.next?.next
+                return nextNext != null
+            }
+
+            override fun next(): T {
+                val result = head?.next
+                head = head?.next
+                return result?.value!!
+            }
+        }
     }
 }
