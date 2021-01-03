086b188b81540806e2a8f28905a65f90020dd140
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-01-03 13:14:14 +0900

    Add ShapeManager

---
+++ libs/graphics/src/main/kotlin/mono/graphics/shape/ShapeManager.kt
@@ -0,0 +1,69 @@
+package mono.graphics.shape
+
+import mono.list.QuickList
+
+/**
+ * A model class which contains all shapes of the app and also defines all shape handling logics.
+ */
+class ShapeManager {
+    // Visible for testing only
+    internal val root: Group = Group(null)
+    private val allShapeMap: MutableMap<Int, AbstractShape> = mutableMapOf(root.id to root)
+
+    private fun getGroup(shapeId: Int?): Group? =
+        if (shapeId == null) root else allShapeMap[shapeId] as? Group
+
+    fun add(shape: AbstractShape) {
+        val group = getGroup(shape.parentId) ?: return
+        group.add(shape)
+        shape.parentId = group.id
+        allShapeMap[shape.id] = shape
+    }
+
+    fun remove(shape: AbstractShape) {
+        val parent = getGroup(shape.parentId) ?: return
+        parent.remove(shape)
+        allShapeMap.remove(shape.id)
+        println("parent $parent ${parent.itemCount} ${parent.parentId} root $root\n")
+        if (parent == root) {
+            return
+        }
+        when (parent.itemCount) {
+            1 -> ungroup(parent)
+            0 -> remove(parent)
+        }
+    }
+
+    fun group(sameParentShapes: List<AbstractShape>) {
+        if (sameParentShapes.size < 2) {
+            // No group 1 or 0 items
+            return
+        }
+        val parentId = sameParentShapes.first().parentId
+        if (sameParentShapes.any { it.parentId != parentId }) {
+            // No group cross group items
+            return
+        }
+        val parent = getGroup(parentId) ?: return
+
+        val group = Group(parentId)
+        parent.add(group, QuickList.AddPosition.After(sameParentShapes.last()))
+
+        for (shape in sameParentShapes) {
+            parent.remove(shape)
+            shape.parentId = group.id
+            group.add(shape)
+        }
+    }
+
+    fun ungroup(group: Group) {
+        val parent = getGroup(group.parentId) ?: return
+
+        for (shape in group.items.reversed()) {
+            group.remove(shape)
+            shape.parentId = parent.parentId
+            parent.add(shape, QuickList.AddPosition.After(group))
+        }
+        remove(group)
+    }
+}
---
+++ libs/graphics/src/test/kotlin/mono/graphics/shape/ShapeManagerTest.kt
@@ -0,0 +1,138 @@
+package mono.graphics.shape
+
+import mono.graphics.geo.Rect
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * A test of [ShapeManager]
+ */
+class ShapeManagerTest {
+    private val target: ShapeManager = ShapeManager()
+
+    @Test
+    fun testAdd() {
+        val shape1 = Rectangle(Rect.ZERO)
+        target.add(shape1)
+        assertEquals(listOf(shape1), target.root.items.toList())
+
+        val group1 = Group(null)
+        target.add(group1)
+        assertEquals(listOf(shape1, group1), target.root.items.toList())
+
+        val shape2 = Rectangle(Rect.ZERO, parentId = group1.id)
+        target.add(shape2)
+        assertEquals(listOf(shape1, group1), target.root.items.toList())
+        assertEquals(listOf(shape2), group1.items.toList())
+
+        val shape3 = Rectangle(Rect.ZERO, parentId = 1000)
+        target.add(shape3)
+        assertEquals(listOf(shape1, group1), target.root.items.toList())
+        assertEquals(listOf(shape2), group1.items.toList())
+    }
+
+    @Test
+    fun testRemove_singleGroupItem_removeGroup() {
+        val group = Group(null)
+        val shape = Rectangle(Rect.ZERO, group.id)
+
+        target.add(group)
+        target.add(shape)
+        target.remove(shape)
+        assertEquals(0, target.root.itemCount)
+    }
+
+    @Test
+    fun testRemove_removeGroupItem_ungroup() {
+        val group = Group(null)
+        val shape1 = Rectangle(Rect.ZERO)
+        val shape2 = Rectangle(Rect.ZERO, group.id)
+        val shape3 = Rectangle(Rect.ZERO, group.id)
+
+        target.add(group)
+        target.add(shape1)
+        target.add(shape2)
+        target.add(shape3)
+
+        target.remove(shape3)
+        assertEquals(listOf(shape2, shape1), target.root.items.toList())
+        assertEquals(target.root.id, shape2.parentId)
+    }
+
+    @Test
+    fun testRemove_removeGroupItem_unchangeRoot() {
+        val group = Group(null)
+        val shape1 = Rectangle(Rect.ZERO)
+        val shape2 = Rectangle(Rect.ZERO, group.id)
+        val shape3 = Rectangle(Rect.ZERO, group.id)
+        val shape4 = Rectangle(Rect.ZERO, group.id)
+
+        target.add(group)
+        target.add(shape1)
+        target.add(shape2)
+        target.add(shape3)
+        target.add(shape4)
+
+        target.remove(shape4)
+        assertEquals(listOf(group, shape1), target.root.items.toList())
+        assertEquals(listOf(shape2, shape3), group.items.toList())
+    }
+
+    @Test
+    fun testGroup_invalid() {
+        val group = Group(null)
+        val shape1 = Rectangle(Rect.ZERO)
+        val shape2 = Rectangle(Rect.ZERO, group.id)
+
+        target.add(group)
+        target.add(shape1)
+        target.add(shape2)
+
+        target.group(listOf(shape1))
+        assertEquals(listOf(group, shape1), target.root.items.toList())
+
+        target.group(listOf(shape1, shape2))
+        assertEquals(listOf(group, shape1), target.root.items.toList())
+        assertEquals(listOf(shape2), group.items.toList())
+    }
+
+    @Test
+    fun testGroup_valid() {
+        val shape0 = Rectangle(Rect.ZERO)
+        val shape1 = Rectangle(Rect.ZERO)
+        val shape2 = Rectangle(Rect.ZERO)
+        val shape3 = Rectangle(Rect.ZERO)
+
+        target.add(shape0)
+        target.add(shape1)
+        target.add(shape2)
+        target.add(shape3)
+        target.group(listOf(shape1, shape2))
+
+        val items = target.root.items.toList()
+
+        assertEquals(3, target.root.itemCount)
+        assertEquals(shape0, items.first())
+        assertEquals(shape3, items.last())
+        val group = items[1] as Group
+        assertEquals(listOf(shape1, shape2), group.items.toList())
+    }
+
+    @Test
+    fun testUngroup() {
+        val group = Group(null)
+        val shape0 = Rectangle(Rect.ZERO)
+        val shape1 = Rectangle(Rect.ZERO, group.id)
+        val shape2 = Rectangle(Rect.ZERO, group.id)
+        val shape3 = Rectangle(Rect.ZERO)
+
+        target.add(shape0)
+        target.add(group)
+        target.add(shape1)
+        target.add(shape2)
+        target.add(shape3)
+
+        target.ungroup(group)
+        assertEquals(listOf(shape0, shape1, shape2, shape3), target.root.items.toList())
+    }
+}
