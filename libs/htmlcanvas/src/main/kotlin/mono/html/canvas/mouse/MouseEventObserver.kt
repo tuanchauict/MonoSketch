0609f8f7166be166cdb00dea907c848d0ad67a4b
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-01-23 18:29:24 +0900

    Add click MousePointer

--- libs/htmlcanvas/src/main/kotlin/mono/html/canvas/mouse/MouseEventObserver.kt
+++ libs/htmlcanvas/src/main/kotlin/mono/html/canvas/mouse/MouseEventObserver.kt
@@ -1,5 +1,6 @@
 package mono.html.canvas.mouse
 
+import mono.graphics.geo.Point
 import mono.livedata.LiveData
 import mono.livedata.MutableLiveData
 import org.w3c.dom.HTMLDivElement
@@ -21,15 +22,21 @@ class MouseEventObserver(container: HTMLDivElement) {
     }
 
     private fun setMouseDownPointer(event: MouseEvent) {
-        mousePointerMutableLiveData.value = MousePointer.Down(event.clientX, event.clientY)
+        mousePointerMutableLiveData.value = MousePointer.Down(event.toPoint())
     }
 
     private fun setMouseUpPointer(event: MouseEvent) {
+        val currentValue = mousePointerLiveData.value
+        val clickPoint = event.toPoint()
         mousePointerMutableLiveData.value = when (mousePointerMutableLiveData.value) {
             is MousePointer.Down,
-            is MousePointer.Move -> MousePointer.Up(event.clientY, event.clientY)
+            is MousePointer.Move -> MousePointer.Up(clickPoint)
             is MousePointer.Up,
+            is MousePointer.Click,
             MousePointer.Idle -> MousePointer.Idle
+        }
+        if (currentValue is MousePointer.Down) {
+            mousePointerMutableLiveData.value = MousePointer.Click(clickPoint)
         }
         mousePointerMutableLiveData.value = MousePointer.Idle
     }
@@ -37,16 +44,23 @@ class MouseEventObserver(container: HTMLDivElement) {
     private fun setMouseMovePointer(event: MouseEvent) {
         mousePointerMutableLiveData.value = when (mousePointerMutableLiveData.value) {
             is MousePointer.Down,
-            is MousePointer.Move -> MousePointer.Move(event.clientX, event.clientY)
+            is MousePointer.Move -> MousePointer.Move(event.toPoint())
             is MousePointer.Up,
+            is MousePointer.Click,
             MousePointer.Idle -> MousePointer.Idle
         }
     }
+
+    private fun MouseEvent.toPoint(): Point = Point(clientX, clientY)
 }
 
+/**
+ * A sealed class which indicates mouse event pointer types.
+ */
 sealed class MousePointer {
     object Idle : MousePointer()
-    data class Down(val left: Int, val top: Int) : MousePointer()
-    data class Move(val left: Int, val top: Int) : MousePointer()
-    data class Up(val left: Int, val top: Int) : MousePointer()
+    data class Down(val point: Point) : MousePointer()
+    data class Move(val point: Point) : MousePointer()
+    data class Up(val point: Point) : MousePointer()
+    data class Click(val point: Point) : MousePointer()
 }
