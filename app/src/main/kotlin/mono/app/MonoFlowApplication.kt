f554de813dd916e7fbd5fcb3fa0d9df4e288ef70
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-01-30 13:18:56 +0900

    Remove unused imports

--- app/src/main/kotlin/mono/app/MonoFlowApplication.kt
+++ app/src/main/kotlin/mono/app/MonoFlowApplication.kt
@@ -2,10 +2,7 @@ package mono.app
 
 import kotlinx.browser.document
 import mono.graphics.bitmap.MonoBitmapManager
-import mono.graphics.board.Highlight
 import mono.graphics.board.MonoBoard
-import mono.graphics.geo.MousePointer
-import mono.graphics.geo.Rect
 import mono.graphics.geo.Size
 import mono.html.canvas.CanvasViewController
 import mono.lifecycle.LifecycleOwner
