e34db4b4db925ad2f59d90ecf0eccaadcccc614f
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-03-11 06:06:32 +0900

    Add keycommand module

---
+++ libs/keycommand/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    implementation(project(":commons"))
+
+    testImplementation(kotlin("test-js"))
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+        binaries.executable()
+    }
+}
