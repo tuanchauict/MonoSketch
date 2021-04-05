1db1426bccd909a2c0792900d8b85d4efc99b680
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-04-05 09:35:46 +0900

    Draw interaction bound with points

--- libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/InteractionCanvasViewController.kt
+++ libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/InteractionCanvasViewController.kt
@@ -1,7 +1,9 @@
 package mono.html.canvas.canvas
 
 import mono.shapebound.InteractionBound
+import mono.shapebound.ScalableInteractionBound
 import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.Path2D
 
 /**
  * A canvas view controller to render the interaction indicators.
@@ -13,8 +15,33 @@ internal class InteractionCanvasViewController(
     var interactionBounds: List<InteractionBound> = emptyList()
 
     override fun drawInternal() {
+        context.strokeStyle = "#6b6b6b"
+        context.fillStyle = "#6b6b6b"
+
         for (bound in interactionBounds) {
-            // TODO: draw
+            when (bound) {
+                is ScalableInteractionBound -> drawScalableInteractionBound(bound)
+            }
         }
+    }
+
+    private fun drawScalableInteractionBound(bound: ScalableInteractionBound) {
+        val path = Path2D().apply {
+            moveTo(drawingInfo.toXPx(bound.left), drawingInfo.toYPx(bound.top))
+            lineTo(drawingInfo.toXPx(bound.right), drawingInfo.toYPx(bound.top))
+            lineTo(drawingInfo.toXPx(bound.right), drawingInfo.toYPx(bound.bottom))
+            lineTo(drawingInfo.toXPx(bound.left), drawingInfo.toYPx(bound.bottom))
+            closePath()
+        }
+        context.stroke(path)
+
+        for (point in bound.interactionPoints) {
+            drawDot(drawingInfo.toXPx(point.left), drawingInfo.toYPx(point.top))
+        }
+    }
+
+    private fun drawDot(xPx: Double, yPx: Double) {
+        val dotSizePx = 6.0
+        context.fillRect(xPx - dotSizePx / 2, yPx - dotSizePx / 2, dotSizePx, dotSizePx)
     }
 }
