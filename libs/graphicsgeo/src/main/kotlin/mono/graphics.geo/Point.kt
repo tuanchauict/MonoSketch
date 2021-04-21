0391bf5fcc842731cfd7e02c71c14bfc024b3841
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-04-22 07:41:18 +0900

    Add more kinds of point
    - Directed point
    - PointF

--- libs/graphicsgeo/src/main/kotlin/mono/graphics.geo/Point.kt
+++ libs/graphicsgeo/src/main/kotlin/mono/graphics.geo/Point.kt
@@ -1,8 +1,8 @@
 package mono.graphics.geo
 
 data class Point(val left: Int, val top: Int) {
-    val row get() = top
-    val column get() = left
+    val row: Int get() = top
+    val column: Int get() = left
 
     operator fun minus(base: Point): Point = Point(left - base.left, top - base.top)
 
@@ -11,5 +11,21 @@ data class Point(val left: Int, val top: Int) {
     companion object {
         val INVALID = Point(Int.MIN_VALUE, Int.MIN_VALUE)
         val ZERO = Point(0, 0)
+    }
+}
+
+data class PointF(val left: Float, val top: Float) {
+    val row: Float get() = top
+
+    val column: Float get() = left
+}
+
+data class DirectedPoint(val direction: Direction, val left: Int, val top: Int) {
+
+    val point: Point
+        get() = Point(left, top)
+
+    enum class Direction {
+        HORIZONTAL, VERTICAL
     }
 }
