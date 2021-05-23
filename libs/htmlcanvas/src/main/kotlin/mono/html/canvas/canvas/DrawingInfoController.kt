39e44676de8e539bc5346c14145d32cef671271e
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-05-23 12:57:29 +0900

    Add font and font size into DrawingInfo and init drawing info with font size

--- libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/DrawingInfoController.kt
+++ libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/DrawingInfoController.kt
@@ -1,20 +1,69 @@
 package mono.html.canvas.canvas
 
+import kotlinx.html.js.canvas
+import kotlinx.html.dom.append
+import mono.common.firstOrNull
 import mono.graphics.geo.Point
 import mono.graphics.geo.Rect
 import mono.graphics.geo.Size
 import mono.graphics.geo.SizeF
+import mono.livedata.MutableLiveData
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.CanvasTextAlign
+import org.w3c.dom.CanvasTextBaseline
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.LEFT
+import org.w3c.dom.MIDDLE
 import kotlin.math.ceil
 
 /**
  * A controller class to manage drawing info for the other canvas.
  */
-internal class DrawingInfoController {
+internal class DrawingInfoController(container: HTMLDivElement) {
+    private val context: CanvasRenderingContext2D
+
+    private val drawingInfoMutableLiveData: MutableLiveData<DrawingInfo>
+
+    init {
+        container.append {
+            canvas(CLASS_NAME) {}
+        }
+        val canvas = container.getElementsByClassName(CLASS_NAME).firstOrNull<HTMLCanvasElement>()!!
+        context = canvas.getContext("2d") as CanvasRenderingContext2D
+        drawingInfoMutableLiveData = MutableLiveData(
+            DrawingInfo(canvasSizePx = Size(canvas.width, canvas.height))
+        )
+        setFont(15)
+    }
+
+    private fun setFont(fontSize: Int) {
+        drawingInfoMutableLiveData.value =
+            drawingInfoMutableLiveData.value.copy(
+                cellSizePx = context.getCellSizePx(fontSize),
+                font = "normal normal normal ${fontSize}px Courier",
+                boldFont = "normal normal 600 ${fontSize}px Courier",
+                fontSize = fontSize
+            )
+    }
+
+    private fun CanvasRenderingContext2D.getCellSizePx(fontSize: Int): SizeF {
+        context.font = font
+        context.textAlign = CanvasTextAlign.LEFT
+        context.textBaseline = CanvasTextBaseline.MIDDLE
+        val metrics = measureText("█")
+        val cWidth = metrics.width
+        val cHeight = fontSize.toDouble()
+        return SizeF(cWidth, cHeight)
+    }
 
     internal data class DrawingInfo(
         val offsetPx: Point = Point.ZERO,
         val cellSizePx: SizeF = SizeF(1.0, 1.0),
-        val canvasSizePx: Size = Size(1, 1)
+        val canvasSizePx: Size = Size(1, 1),
+        val font: String = "",
+        val boldFont: String = "",
+        val fontSize: Int = 0
     ) {
         val boundPx: Rect = Rect(offsetPx, canvasSizePx)
 
@@ -33,5 +82,9 @@ internal class DrawingInfoController {
         fun toYPx(row: Double): Double = offsetPx.top + cellSizePx.height * row
         fun toBoardRow(yPx: Int): Int = ((yPx - offsetPx.top) / cellSizePx.height).toInt()
         fun toBoardColumn(xPx: Int): Int = ((xPx - offsetPx.left) / cellSizePx.width).toInt()
+    }
+
+    companion object {
+        private const val CLASS_NAME = "drawing-info"
     }
 }
