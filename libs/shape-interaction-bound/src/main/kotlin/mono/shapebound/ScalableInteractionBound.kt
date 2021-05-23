218f96e4920221ce58f31f0a16a82d4f98cde3dc
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-05-23 18:39:27 +0900

    Move classes within InteractionBound to separated classes

---
+++ libs/shape-interaction-bound/src/main/kotlin/mono/shapebound/ScalableInteractionBound.kt
@@ -0,0 +1,30 @@
+package mono.shapebound
+
+import mono.graphics.geo.Rect
+
+/**
+ * A class which defines interaction bound for scalable shapes.
+ */
+class ScalableInteractionBound(
+    targetedShapeId: Int,
+    shapeBound: Rect
+) : InteractionBound() {
+    val left: Double = shapeBound.left.toDouble()
+    val top: Double = shapeBound.top.toDouble()
+    val right: Double = shapeBound.right + 1.0
+    val bottom: Double = shapeBound.bottom + 1.0
+
+    private val horizontalMiddle: Double = (left + right) / 2.0
+    private val verticalMiddle: Double = (top + bottom) / 2.0
+
+    override val interactionPoints: List<ScaleInteractionPoint> = listOf(
+        ScaleInteractionPoint.TopLeft(targetedShapeId, left, top),
+        ScaleInteractionPoint.TopMiddle(targetedShapeId, horizontalMiddle, top),
+        ScaleInteractionPoint.TopRight(targetedShapeId, right, top),
+        ScaleInteractionPoint.MiddleLeft(targetedShapeId, left, verticalMiddle),
+        ScaleInteractionPoint.MiddleRight(targetedShapeId, right, verticalMiddle),
+        ScaleInteractionPoint.BottomLeft(targetedShapeId, left, bottom),
+        ScaleInteractionPoint.BottomMiddle(targetedShapeId, horizontalMiddle, bottom),
+        ScaleInteractionPoint.BottomRight(targetedShapeId, right, bottom),
+    )
+}
