1f2df219c0b5de2934b7fce7abd9b2e6b94e0ca5
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-06-27 23:19:13 +0900

    Implement duplicate selected shapes

--- libs/statemanager/src/main/kotlin/mono/state/ClipboardManager.kt
+++ libs/statemanager/src/main/kotlin/mono/state/ClipboardManager.kt
@@ -1,10 +1,13 @@
 package mono.state
 
+import mono.graphics.geo.Point
 import mono.lifecycle.LifecycleOwner
 import mono.shape.add
 import mono.shape.clipboard.ShapeClipboardManager
 import mono.shape.remove
+import mono.shape.serialization.AbstractSerializableShape
 import mono.shape.shape.AbstractShape
+import mono.shape.shape.Group
 import mono.state.command.CommandEnvironment
 
 /**
@@ -40,6 +43,36 @@ internal class ClipboardManager(
     }
 
     fun duplicateSelectedShapes() {
-        TODO("Implement this method")
+        if (selectedShapes.isEmpty()) {
+            return
+        }
+        val currentSelectedShapes = selectedShapes
+        val serializableShapes = currentSelectedShapes.map { it.toSerializableShape() }
+        val minLeft = currentSelectedShapes.minOf { it.bound.left }
+        val minTop = currentSelectedShapes.minOf { it.bound.top }
+
+        commandEnvironment.clearSelectedShapes()
+        insertShapes(minLeft + 1, minTop + 1, serializableShapes)
+    }
+
+    private fun insertShapes(
+        left: Int,
+        top: Int,
+        serializableShapes: List<AbstractSerializableShape>
+    ) {
+        val currentParentId = commandEnvironment.workingParentGroup.id
+        val shapes = serializableShapes.map { Group.toShape(currentParentId, it) }
+        val minLeft = shapes.minOf { it.bound.left }
+        val minTop = shapes.minOf { it.bound.top }
+
+        val offset = Point(minLeft - left, minTop - top)
+        for (shape in shapes) {
+            val shapeBound = shape.bound
+            val newShapeBound = shapeBound.copy(position = shapeBound.position.minus(offset))
+            shape.setBound(newShapeBound)
+
+            commandEnvironment.shapeManager.add(shape)
+            commandEnvironment.addSelectedShape(shape)
+        }
     }
 }
