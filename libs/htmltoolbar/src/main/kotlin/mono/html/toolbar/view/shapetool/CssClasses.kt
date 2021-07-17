b4f3c7a58ef2d3b4c58c1a832cb9f98cd7d82424
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-07-17 12:08:40 +0900

    Add enum for CSS classes

---
+++ libs/htmltoolbar/src/main/kotlin/mono/html/toolbar/view/shapetool/CssClasses.kt
@@ -0,0 +1,34 @@
+package mono.html.toolbar.view.shapetool
+
+internal fun classes(vararg cls: Class?): String =
+    cls.filterNotNull().joinToString(" ") { it.value }
+
+enum class Class(val value: String) {
+    ADD_BOTTOM_SPACE("add-bottom-space"),
+    ADD_RIGHT_SPACE("add-right-space"),
+    CENTER_EVEN_SPACE("center-even-space"),
+    CENTER_VERTICAL("center-vertical"),
+    COLUMN("tcolumn"),
+    DISABLED("disabled"),
+    DIVIDER("divider"),
+    GRID("grid"),
+    HALF("half"),
+    ICON_BUTTON("icon-button"),
+    INLINE_TITLE("inline-title"),
+    INPUT_CHECK_BOX("tool-input-checkbox"),
+    INPUT_TEXT("tool-input-text"),
+    MEDIUM("medium"),
+    MONOFONT("monofont"),
+    ROW("trow"),
+    QUARTER("quarter"),
+    SELECTED("selected"),
+    SECTION("tsection"),
+    SECTION_TITLE("tsection-title"),
+    SHORT("short"),
+    SMALL("small"),
+    SMALL_SPACE("small-space"),
+    TOOL("tool"),
+    TOOL_TITLE("tool-title");
+
+    infix fun x(isAccepted: Boolean): Class? = this.takeIf { isAccepted }
+}
