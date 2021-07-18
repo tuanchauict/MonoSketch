babfda718a7f8fc1a939c4de32183e0d1a6a9608
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-07-18 12:57:31 +0900

    Refactor AppearanceTool for view controller

--- libs/htmltoolbar/src/main/kotlin/mono/html/toolbar/view/shapetool/AppearanceTool.kt
+++ libs/htmltoolbar/src/main/kotlin/mono/html/toolbar/view/shapetool/AppearanceTool.kt
@@ -9,82 +9,97 @@ import kotlinx.html.js.span
 import mono.html.toolbar.view.Tag
 import mono.html.toolbar.view.shapetool.Class.ADD_RIGHT_SPACE
 import mono.html.toolbar.view.shapetool.Class.COLUMN
-import mono.html.toolbar.view.shapetool.Class.DISABLED
 import mono.html.toolbar.view.shapetool.Class.ICON_BUTTON
 import mono.html.toolbar.view.shapetool.Class.INPUT_CHECK_BOX
-import mono.html.toolbar.view.shapetool.Class.SELECTED
 import mono.html.toolbar.view.shapetool.Class.SMALL
 import mono.html.toolbar.view.shapetool.Class.TOOL_TITLE
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.HTMLInputElement
 
-internal fun Tag.AppearanceSection() {
-    Section("APPEARANCE") {
-        GridTextIconOptions(
-            "Fill",
-            isSelected = false,
-            selectedPosition = 0,
-            listOf(" ", "█", "▒", "░", "▚")
-        )
-        GridTextIconOptions(
-            "Border",
-            isSelected = true,
-            selectedPosition = 0,
-            listOf("─", "━", "═", "█", "▒", "░")
-        )
-        GridTextIconOptions(
-            "Start head",
-            isSelected = true,
-            selectedPosition = 0,
-            listOf("▶", "■", "○", "◎", "●")
-        )
-        GridTextIconOptions(
-            "End head",
-            isSelected = true,
-            selectedPosition = 0,
-            listOf("▶", "■", "○", "◎", "●")
-        )
+internal abstract class AppearanceSectionViewController(
+    rootView: HTMLDivElement
+) : ToolViewController(rootView)
+
+private class AppearanceSectionViewControllerImpl(
+    rootView: HTMLDivElement,
+    private val fillTool: AppearanceToolViewController,
+    private val borderTool: AppearanceToolViewController,
+    private val strokeTool: AppearanceToolViewController,
+    private val startHeadTool: AppearanceToolViewController,
+    private val endHeadTool: AppearanceToolViewController
+) : AppearanceSectionViewController(rootView)
+
+private class AppearanceToolViewController(
+    rootView: HTMLDivElement,
+    private val checkBox: HTMLInputElement,
+    private val options: List<HTMLElement>
+) : ToolViewController(rootView)
+
+internal fun Tag.AppearanceSection(
+    fillOptions: List<String> = listOf(" ", "█", "▒", "░", "▚"),
+    borderOptions: List<String> = listOf("─", "━", "═"),
+    headOptions: List<String> = listOf("▶", "■", "○", "◎", "●")
+): AppearanceSectionViewController {
+    val tools = mutableListOf<AppearanceToolViewController>()
+    val rootView = Section("APPEARANCE") {
+        tools += GridTextIconOptions("Fill", fillOptions)
+        tools += GridTextIconOptions("Border", borderOptions)
+        tools += GridTextIconOptions("Stroke", borderOptions)
+        tools += GridTextIconOptions("Start head", headOptions)
+        tools += GridTextIconOptions("End head", headOptions)
     }
+    return AppearanceSectionViewControllerImpl(
+        rootView,
+        tools[0],
+        tools[1],
+        tools[2],
+        tools[3],
+        tools[4]
+    )
 }
 
 private fun Tag.GridTextIconOptions(
     title: String,
-    isSelected: Boolean,
-    selectedPosition: Int,
     options: List<String>
-) {
-    Tool(hasMoreBottomSpace = true) {
+): AppearanceToolViewController {
+    var checkBox: HTMLInputElement? = null
+    var optionElements: List<HTMLElement>? = null
+    val rootView = Tool(hasMoreBottomSpace = true) {
         Row {
-            CheckColumn(isSelected)
-            OptionsColumn(title, isSelected, selectedPosition, options)
+            checkBox = CheckColumn()
+            optionElements = OptionsColumn(title, options)
         }
     }
+    return AppearanceToolViewController(rootView, checkBox!!, optionElements!!)
 }
 
-private fun Tag.CheckColumn(isChecked: Boolean) {
+private fun Tag.CheckColumn(): HTMLInputElement {
+    var checkBox: HTMLInputElement? = null
     Column(hasMoreRightSpace = true) {
         Row {
-            input(InputType.checkBox, classes = classes(INPUT_CHECK_BOX)) {
-                checked = isChecked
-            }
+            checkBox = input(InputType.checkBox, classes = classes(INPUT_CHECK_BOX))
         }
     }
+    return checkBox!!
 }
 
 private fun Tag.OptionsColumn(
     title: String,
-    isEnabled: Boolean,
-    selectedPosition: Int,
     options: List<String>
-) {
+): List<HTMLElement> {
+    val optionElements = mutableListOf<HTMLElement>()
     Column {
         Row {
             span(classes(TOOL_TITLE)) { +title }
         }
         Row(isMonoFont = true, isGrid = true) {
-            options.forEachIndexed { index, option ->
-                TextButton(option, selectedPosition == index, isEnabled)
+            for (option in options) {
+                optionElements += span(classes(ICON_BUTTON, SMALL)) { +option }
             }
         }
     }
+    return optionElements
 }
 
 private fun Tag.Column(hasMoreRightSpace: Boolean = false, block: Tag.() -> Unit) {
@@ -92,15 +107,4 @@ private fun Tag.Column(hasMoreRightSpace: Boolean = false, block: Tag.() -> Unit
     div(columnClasses) {
         block()
     }
-}
-
-private fun Tag.TextButton(text: String, isSelected: Boolean, isEnabled: Boolean) {
-    val buttonClasses =
-        classes(
-            ICON_BUTTON,
-            SMALL,
-            DISABLED x !isEnabled,
-            SELECTED x (isSelected && isEnabled)
-        )
-    span(buttonClasses) { +text }
 }
