645d9b5da33944878843cff6c8b35f267a39723e
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-07-22 14:44:12 +0900

    Floor cell width to avoid having a tiny space at 8*nth columns

--- libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/DrawingInfoController.kt
+++ libs/htmlcanvas/src/main/kotlin/mono/html/canvas/canvas/DrawingInfoController.kt
@@ -64,7 +64,7 @@ internal class DrawingInfoController(container: HTMLDivElement) {
         context.font = font
         context.textAlign = CanvasTextAlign.LEFT
         context.textBaseline = CanvasTextBaseline.MIDDLE
-        val cWidth = fontSize.toDouble() / 1.6
+        val cWidth = floor(fontSize.toDouble() / 1.6)
         val cHeight = fontSize.toDouble()
         return SizeF(cWidth, cHeight)
     }
