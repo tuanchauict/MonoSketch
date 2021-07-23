643ef94c63683ff61cc5cb85051eb89b066e17d6
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-07-23 13:08:41 +0900

    Add RectangleBorderStyle and PredefinedRectangleBorderStyle

--- libs/shape-extra-manager/src/main/kotlin/mono/shape/extra/manager/ShapeExtraManager.kt
+++ libs/shape-extra-manager/src/main/kotlin/mono/shape/extra/manager/ShapeExtraManager.kt
@@ -1,6 +1,8 @@
 package mono.shape.extra.manager
 
+import mono.shape.extra.manager.model.RectangleBorderStyle
 import mono.shape.extra.manager.model.RectangleFillStyle
+import mono.shape.extra.manager.predefined.PredefinedRectangleBorderStyle
 import mono.shape.extra.manager.predefined.PredefinedRectangleFillStyle
 
 /**
@@ -11,8 +13,15 @@ class ShapeExtraManager {
         PredefinedRectangleFillStyle.PREDEFINED_STYLE_MAP[id]
             ?: PredefinedRectangleFillStyle.NOFILLED_STYLE
 
-    fun getAllPredefinedStyles(): List<RectangleFillStyle> =
+    fun getAllPredefinedRectangleFillStyles(): List<RectangleFillStyle> =
         PredefinedRectangleFillStyle.PREDEFINED_STYLES
+
+    fun getRectangleBorderStyle(id: String = NO_ID): RectangleBorderStyle =
+        PredefinedRectangleBorderStyle.PREDEFINED_STYLE_MAP[id]
+            ?: PredefinedRectangleBorderStyle.NO_BORDER
+
+    fun getAllPredefinedRectangleBorderStyles(): List<RectangleBorderStyle> =
+        PredefinedRectangleBorderStyle.PREDEFINED_STYLES
 
     companion object {
         private const val NO_ID = ""
