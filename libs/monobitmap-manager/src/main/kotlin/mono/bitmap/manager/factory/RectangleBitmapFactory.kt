57e74e42142681992151388ddfd5ed59218324b4
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-08-08 17:12:54 +0900

    Not draw border when 1 size is 1

--- libs/monobitmap-manager/src/main/kotlin/mono/bitmap/manager/factory/RectangleBitmapFactory.kt
+++ libs/monobitmap-manager/src/main/kotlin/mono/bitmap/manager/factory/RectangleBitmapFactory.kt
@@ -8,38 +8,22 @@ import mono.shape.shape.extra.RectangleExtra
 object RectangleBitmapFactory {
 
     private val NINE_PATCH_DOT = NinePatchDrawable(
-        NinePatchDrawable.Pattern.fromText("•")
-    )
-
-    private val NINE_PATCH_HORIZONTAL_LINE = NinePatchDrawable(
-        NinePatchDrawable.Pattern.fromText("─"),
-        horizontalRepeatableRange = NinePatchDrawable.RepeatableRange.Repeat(0, 0),
-    )
-
-    private val NINE_PATCH_VERTICAL_LINE = NinePatchDrawable(
-        NinePatchDrawable.Pattern.fromText("│"),
-        verticalRepeatableRange = NinePatchDrawable.RepeatableRange.Repeat(0, 0)
+        NinePatchDrawable.Pattern.fromText("▫")
     )
 
     fun toBitmap(size: Size, extra: RectangleExtra): MonoBitmap {
-        val fillDrawable = when {
-            size.width == 1 && size.height == 1 -> null
-            size.width == 1 -> null
-            size.height == 1 -> null
-            else -> extra.fillStyle.drawable
-        }
+        val fillDrawable = extra.fillStyle.drawable
         val borderDrawable = when {
+            size.width > 1 && size.height > 1 -> extra.borderStyle.drawable
             size.width == 1 && size.height == 1 -> NINE_PATCH_DOT
-            size.width == 1 -> NINE_PATCH_VERTICAL_LINE
-            size.height == 1 -> NINE_PATCH_HORIZONTAL_LINE
-            else -> extra.borderStyle.drawable
+            else -> null
         }
 
         val bitmapBuilder = MonoBitmap.Builder(size.width, size.height)
-        if (fillDrawable != null) {
-            bitmapBuilder.fill(0, 0, fillDrawable.toBitmap(size.width, size.height))
+        bitmapBuilder.fill(0, 0, fillDrawable.toBitmap(size.width, size.height))
+        if (borderDrawable != null) {
+            bitmapBuilder.fill(0, 0, borderDrawable.toBitmap(size.width, size.height))
         }
-        bitmapBuilder.fill(0, 0, borderDrawable.toBitmap(size.width, size.height))
 
         return bitmapBuilder.toBitmap()
     }
