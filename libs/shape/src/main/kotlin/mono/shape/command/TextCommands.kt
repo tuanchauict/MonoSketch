60789e5040ffb27cd85bd50b188063ad6dc3ffb1
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-09-15 06:52:17 +0900

    Add isTextEditing flag to Text

--- libs/shape/src/main/kotlin/mono/shape/command/TextCommands.kt
+++ libs/shape/src/main/kotlin/mono/shape/command/TextCommands.kt
@@ -21,6 +21,9 @@ class ChangeText(
     }
 }
 
+/**
+ * A [Command] for making an uneditable text to be editable.
+ */
 class MakeTextEditable(private val target: Text) : Command() {
     override fun getDirectAffectedParent(shapeManager: ShapeManager): Group? =
         shapeManager.getGroup(target.parentId)
@@ -28,6 +31,23 @@ class MakeTextEditable(private val target: Text) : Command() {
     override fun execute(shapeManager: ShapeManager, parent: Group) {
         val currentVersion = target.versionCode
         target.makeTextEditable()
+        parent.update { currentVersion != target.versionCode }
+    }
+}
+
+/**
+ * A [Command] for updating text shape's text editing mode.
+ */
+class UpdateTextEditingMode(
+    private val target: Text,
+    private val isEditing: Boolean
+) : Command() {
+    override fun getDirectAffectedParent(shapeManager: ShapeManager): Group? =
+        shapeManager.getGroup(target.parentId)
+
+    override fun execute(shapeManager: ShapeManager, parent: Group) {
+        val currentVersion = target.versionCode
+        target.setTextEditingMode(isEditing)
         parent.update { currentVersion != target.versionCode }
     }
 }
