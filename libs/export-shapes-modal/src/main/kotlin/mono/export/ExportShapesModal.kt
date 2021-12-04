5427da8bd594081575461fb20c4adb35cc76056f
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-12-04 16:24:25 +0900

    Rewrite export text modal with simple structure

--- libs/export-shapes-modal/src/main/kotlin/mono/export/ExportShapesModal.kt
+++ libs/export-shapes-modal/src/main/kotlin/mono/export/ExportShapesModal.kt
@@ -3,15 +3,12 @@
 package mono.export
 
 import kotlinx.browser.document
-import kotlinx.dom.addClass
-import mono.html.A
 import mono.html.Div
-import mono.html.Heading
-import mono.html.HeadingLevel
 import mono.html.Pre
+import mono.html.Span
+import mono.html.Svg
+import mono.html.SvgPath
 import mono.html.TextArea
-import mono.html.px
-import mono.html.styleOf
 import mono.html.setAttributes
 import mono.html.setOnClickListener
 import org.w3c.dom.Element
@@ -21,96 +18,80 @@ import org.w3c.dom.Element
  * text.
  */
 internal class ExportShapesModal {
-    fun show(content: String) = createModalContent(content)
+    private var root: Element? = null
+    fun show(content: String) {
+        root = document.body?.Div(classes = "export-text") {
+            Div(classes = "export-text__modal") {
+                CloseButton()
+                Span(text = "Export", classes = "export-text__title")
+                Content(content)
 
-    private fun createModalContent(content: String) {
-        val body = document.body ?: return
-        with(body) {
-            val rootView = Div("modal fade") {
-                id = MODAL_ID
-                setAttributes(
-                    "tabindex" to "-1",
-                    "aria-labelledby" to "export-shape-modal-title",
-                    "aria-hidden" to "true"
-                )
-
-                Div {
-                    addClass(
-                        "modal-dialog",
-                        "modal-dialog-scrollable",
-                        "modal-dialog-centered",
-                        "modal-xl",
-                        "modal-fullscreen-lg-down"
-                    )
-                    setAttributes("role" to "document")
-
-                    Div("modal-content") {
-                        Header()
-                        Body(content)
-                        Footer(content)
-                    }
-                }
-            }
-            rootView.addEventListener(
-                "hidden.bs.modal",
-                {
-                    rootView.remove()
-                }
-            )
-        }
-        js(
-            """
-            var modal = new bootstrap.Modal(document.getElementById("$MODAL_ID"));
-            modal.show();
-            """
-        )
-    }
-
-    private fun Element.Header() {
-        Div("modal-header") {
-            Heading(HeadingLevel.H5, "modal-title", "Export") {
-                id = "export-shape-modal-title"
-            }
-            A(classes = "btn-close") {
-                setAttributes(
-                    "type" to "button",
-                    "data-bs-dismiss" to "modal",
-                    "aria-label" to "Close"
-                )
-            }
-        }
-    }
-
-    private fun Element.Body(content: String) {
-        Div("modal-body") {
-            Pre(text = content) {
-                setAttributes(
-                    "contenteditable" to true,
-                    "style" to styleOf(
-                        "height" to "100%",
-                        "min-height" to 160.px,
-                        "line-height" to 15.px,
-                        "font-size" to 12.px,
-                        "outline" to "none"
-                    )
-                )
-            }
-        }
-    }
-
-    private fun Element.Footer(content: String) {
-        Div("modal-footer") {
-            val textBox = TextArea(classes = "hidden", content = content)
-            A(classes = "btn btn-outline-primary btn-sm", text = "Copy") {
                 setOnClickListener {
-                    textBox.select()
-                    document.execCommand("copy")
+                    it.stopPropagation()
                 }
+            }
+
+            setOnClickListener { dismiss() }
+        }
+    }
+
+    private fun Element.CloseButton() {
+        Span(classes = "export-text__close") {
+            SvgIcon(16) {
+                /* ktlint-disable max-line-length */
+                SvgPath("M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z")
+                SvgPath("M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z")
+                /* ktlint-enable max-line-length */
+            }
+
+            setOnClickListener { dismiss() }
+        }
+    }
+
+    private fun Element.Content(content: String) {
+        Div(classes = "export-text__content") {
+            val textContent = Pre(text = content) {
+                setAttributes("contenteditable" to true)
+            }
+            val textBox = TextArea(classes = "hidden", content = content)
+            CopyButton {
+                textBox.value = textContent.innerText
+                textBox.select()
+                document.execCommand("copy")
             }
         }
     }
 
-    companion object {
-        private const val MODAL_ID = "export-shape-modal"
+    private fun Element.CopyButton(copyContent: () -> Unit) {
+        Span(classes = "export-text__copy") {
+            SvgIcon(24, 24) {
+                /* ktlint-disable max-line-length */
+                SvgPath("M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z")
+                /* ktlint-enable max-line-length */
+            }
+
+            setAttributes("title" to "Copy")
+            setOnClickListener { copyContent() }
+        }
+    }
+
+    private fun Element.SvgIcon(size: Int, pathBlock: Element.() -> Unit = {}) =
+        SvgIcon(size, size, pathBlock)
+
+    private fun Element.SvgIcon(width: Int, height: Int, pathBlock: Element.() -> Unit) {
+        Svg {
+            setAttributes(
+                "width" to width,
+                "height" to height,
+                "fill" to "currentColor",
+                "viewBox" to "0 0 $width $height"
+            )
+
+            pathBlock()
+        }
+    }
+
+    private fun dismiss() {
+        root?.remove()
     }
 }
