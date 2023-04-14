6a568d6285fa245d25b0d42fe30738893b751775
Tuan Chau <tuanchau.ict@gmail.com>
2023-04-15 07:26:05 +0900

    Use SvgIcon for ExportShapesModal

--- libs/export-shapes-modal/src/main/kotlin/mono/export/ExportShapesModal.kt
+++ libs/export-shapes-modal/src/main/kotlin/mono/export/ExportShapesModal.kt
@@ -12,7 +12,7 @@ import mono.common.setTimeout
 import mono.html.Div
 import mono.html.Pre
 import mono.html.Span
-import mono.html.Svg
+import mono.html.SvgIcon
 import mono.html.SvgPath
 import mono.html.TextArea
 import mono.html.setAttributes
@@ -77,32 +77,13 @@ internal class ExportShapesModal {
 
     private fun Element.CopyButton(copyContent: () -> Unit) {
         Span(classes = "export-text__copy") {
-            SvgIcon(24, 24) {
-                /* ktlint-disable max-line-length */
-                SvgPath(
-                    "M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
-                )
-                /* ktlint-enable max-line-length */
-            }
+            SvgIcon(
+                24,
+                "M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" // ktlint-disable max-line-length
+            )
 
             setAttributes("title" to "Copy")
             setOnClickListener { copyContent() }
-        }
-    }
-
-    private fun Element.SvgIcon(size: Int, pathBlock: Element.() -> Unit = {}) =
-        SvgIcon(size, size, pathBlock)
-
-    private fun Element.SvgIcon(width: Int, height: Int, pathBlock: Element.() -> Unit) {
-        Svg {
-            setAttributes(
-                "width" to width,
-                "height" to height,
-                "fill" to "currentColor",
-                "viewBox" to "0 0 $width $height"
-            )
-
-            pathBlock()
         }
     }
 
