8519ce32d54b7bb7866109187031d7876988dc6a
Tuan Chau <tuanchau.ict@gmail.com>
2023-04-30 13:11:40 +0900

    Show manage project modal

--- libs/ui-toolbar/src/main/kotlin/mono/html/toolbar/NavBarViewController.kt
+++ libs/ui-toolbar/src/main/kotlin/mono/html/toolbar/NavBarViewController.kt
@@ -38,6 +38,7 @@ class NavBarViewController(
                 WorkingFileToolbar(
                     lifecycleOwner,
                     fileNameLiveData,
+                    workspaceDao,
                     actionManager::setOneTimeAction
                 )
             }
