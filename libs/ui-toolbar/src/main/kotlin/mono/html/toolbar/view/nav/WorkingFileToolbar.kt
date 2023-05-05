5c824432bd4275ea454fab09889f32a28cc4b6e7
Tuan Chau <tuanchau.ict@gmail.com>
2023-05-05 20:22:38 +0900

    Also show rename project when creating new project

--- libs/ui-toolbar/src/main/kotlin/mono/html/toolbar/view/nav/WorkingFileToolbar.kt
+++ libs/ui-toolbar/src/main/kotlin/mono/html/toolbar/view/nav/WorkingFileToolbar.kt
@@ -14,6 +14,7 @@ import mono.html.modal.compose.ProjectItem
 import mono.html.modal.compose.showRecentProjectsModal
 import mono.html.toolbar.view.nav.DropDownItem.Forwarding
 import mono.html.toolbar.view.nav.DropDownItem.ManageProject
+import mono.html.toolbar.view.nav.DropDownItem.NewProject
 import mono.html.toolbar.view.nav.DropDownItem.Rename
 import mono.html.toolbar.view.nav.workingfile.showRenameProjectModal
 import mono.store.dao.workspace.WorkspaceDao
@@ -36,14 +37,12 @@ internal fun WorkingFileToolbar(
             when (it) {
                 is Forwarding -> onActionSelected(it.actionType)
 
-                Rename -> showRenameProjectModal(
-                    projectNameState.value,
-                    "#$WORKING_PROJECT_ID"
-                ) { newName ->
-                    if (newName.isNotEmpty()) {
-                        onActionSelected(OneTimeActionType.RenameCurrentProject(newName))
-                    }
+                NewProject -> {
+                    onActionSelected(OneTimeActionType.NewProject)
+                    renameProject(projectNameState, onActionSelected)
                 }
+
+                Rename -> renameProject(projectNameState, onActionSelected)
 
                 ManageProject -> onManageProjectClick(workspaceDao, onActionSelected)
             }
@@ -84,7 +83,7 @@ private fun CurrentProject(title: String, showProjectMenu: (Element) -> Unit) {
 
 private fun showWorkingFileMenu(anchor: Element, onItemSelected: (DropDownItem) -> Unit) {
     val items = listOf(
-        DropDownMenu.Item.Text("New project", Forwarding(OneTimeActionType.NewProject)),
+        DropDownMenu.Item.Text("New project", NewProject),
         DropDownMenu.Item.Text("Manage projects", ManageProject),
         DropDownMenu.Item.Divider(),
         DropDownMenu.Item.Text("Rename", Rename),
@@ -112,11 +111,26 @@ private fun onManageProjectClick(
     }
 }
 
+private fun renameProject(
+    projectNameState: State<String>,
+    onActionSelected: (OneTimeActionType) -> Unit
+) {
+    showRenameProjectModal(
+        projectNameState.value,
+        "#$WORKING_PROJECT_ID"
+    ) { newName ->
+        if (newName.isNotEmpty()) {
+            onActionSelected(OneTimeActionType.RenameCurrentProject(newName))
+        }
+    }
+}
+
 /**
  * A sealed interface for dropdown menu items of working file.
  */
 private sealed interface DropDownItem {
     class Forwarding(val actionType: OneTimeActionType) : DropDownItem
+    object NewProject : DropDownItem
     object Rename : DropDownItem
     object ManageProject : DropDownItem
 }
