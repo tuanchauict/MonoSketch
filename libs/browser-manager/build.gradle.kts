8c261ee0aecee50ad071cd557cd368cd3a0c4016
Tuan Chau <tuanchau.ict@gmail.com>
2023-05-14 13:05:36 +0900

    Add browser manager module

---
+++ libs/browser-manager/build.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023, tuanchauict
+ */
+
+plugins {
+    kotlin("js")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib.js)
+    testImplementation(libs.kotlin.test.js)
+}
+
+val compilerType: org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType by ext
+kotlin {
+    js(compilerType) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+    }
+}
