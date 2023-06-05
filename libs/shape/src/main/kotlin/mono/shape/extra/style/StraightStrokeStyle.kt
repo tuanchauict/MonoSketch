f59309fb6219d0c98e13008373cd52ecc5c1756b
Tuan Chau <tuanchau.ict@gmail.com>
2023-06-06 07:13:16 +0900

    Make StraightStrokeStyle a data class

--- libs/shape/src/main/kotlin/mono/shape/extra/style/StraightStrokeStyle.kt
+++ libs/shape/src/main/kotlin/mono/shape/extra/style/StraightStrokeStyle.kt
@@ -7,7 +7,7 @@ package mono.shape.extra.style
 /**
  * A class for defining a stroke style of straight line.
  */
-class StraightStrokeStyle(
+data class StraightStrokeStyle(
     val id: String,
     val displayName: String,
     val horizontal: Char,
