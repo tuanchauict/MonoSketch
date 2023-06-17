ee3265ae337bb0deab16876dc65e5b440a56d109
Tuan Chau <tuanchau.ict@gmail.com>
2023-06-18 08:59:37 +0900

    Move shape along with box

--- libs/shape/src/main/kotlin/mono/shape/command/GeneralShapeCommands.kt
+++ libs/shape/src/main/kotlin/mono/shape/command/GeneralShapeCommands.kt
@@ -6,9 +6,11 @@ package mono.shape.command
 
 import mono.graphics.geo.Rect
 import mono.shape.ShapeManager
+import mono.shape.connector.ShapeConnectorUseCase.getPointInNewBound
 import mono.shape.extra.ShapeExtra
 import mono.shape.shape.AbstractShape
 import mono.shape.shape.Group
+import mono.shape.shape.Line
 
 class ChangeBound(private val target: AbstractShape, private val newBound: Rect) : Command() {
     override fun getDirectAffectedParent(shapeManager: ShapeManager): Group? =
@@ -17,9 +19,18 @@ class ChangeBound(private val target: AbstractShape, private val newBound: Rect)
     override fun execute(shapeManager: ShapeManager, parent: Group) {
         val currentVersion = target.versionCode
         target.setBound(newBound)
-        if (currentVersion != target.versionCode) {
-            parent.update { true }
+        if (currentVersion == target.versionCode) {
+            return
         }
+
+        val connectors = shapeManager.connectorManager.getConnectors(target)
+        for (connector in connectors) {
+            val anchorPointUpdate =
+                Line.AnchorPointUpdate(connector.anchor, connector.getPointInNewBound(newBound))
+            connector.line.moveAnchorPoint(anchorPointUpdate, isReduceRequired = false)
+        }
+
+        parent.update { true }
     }
 }
 
