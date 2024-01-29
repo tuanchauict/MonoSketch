c38cd34803b8c5f0c8db028efa104e8f47d0eebe
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-01-29 10:17:15 +0900

    Add window view model for observing window state

--- monosketch-svelte/src/main.ts
+++ monosketch-svelte/src/main.ts
@@ -3,10 +3,15 @@ import '$style/main.scss';
 import App from './App.svelte';
 import { AppContext } from '$app/app-context';
 import { APP_CONTEXT } from '$mono/common/constant';
+import { WindowViewModel } from '$mono/window/window-viewmodel';
 
 const appContext = new AppContext();
 const context = new Map();
 context.set(APP_CONTEXT, appContext);
+
+window.onresize = () => {
+    WindowViewModel.windowSizeUpdateEventFlow.value = true;
+}
 
 const app = new App({
     // @ts-ignore
