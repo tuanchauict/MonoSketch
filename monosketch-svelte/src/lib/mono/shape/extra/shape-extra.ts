8ee08fe8c1115a5510ae53927d2ef2d1edca79ad
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-12-05 21:40:13 +0900

    Merge extra manager into shape-extra due to circular dependencies

--- monosketch-svelte/src/lib/mono/shape/extra/shape-extra.ts
+++ monosketch-svelte/src/lib/mono/shape/extra/shape-extra.ts
@@ -1,6 +1,10 @@
 import type { Comparable } from "$libs/comparable";
-import { ShapeExtraManager } from "$mono/shape/extra/extra-manager";
-import { PredefinedStraightStrokeStyle } from "$mono/shape/extra/predefined-styles";
+import { Flow } from "$libs/flow";
+import {
+    PredefinedAnchorChar,
+    PredefinedRectangleFillStyle,
+    PredefinedStraightStrokeStyle,
+} from "$mono/shape/extra/predefined-styles";
 import {
     AnchorChar,
     RectangleBorderCornerPattern,
@@ -272,13 +276,15 @@ export class TextExtra implements ShapeExtra {
         );
     }
 
-    static readonly NO_BOUND = new TextExtra(
-        ShapeExtraManager.defaultRectangleExtra.copy({
-            isFillEnabled: false,
-            isBorderEnabled: false,
-        }),
-        new TextAlign(TextHorizontalAlign.LEFT, TextVerticalAlign.TOP),
-    );
+    static noBound(): TextExtra {
+        return new TextExtra(
+            ShapeExtraManager.defaultRectangleExtra.copy({
+                isFillEnabled: false,
+                isBorderEnabled: false,
+            }),
+            new TextAlign(TextHorizontalAlign.LEFT, TextVerticalAlign.TOP),
+        );
+    }
 
     static withDefault(): TextExtra {
         return new TextExtra(
@@ -288,3 +294,175 @@ export class TextExtra implements ShapeExtra {
     }
 }
 
+/**
+ * A manager for shape extras.
+ */
+export class ShapeExtraManager {
+    static defaultRectangleExtra: RectangleExtra = RectangleExtra.create(
+        false,
+        PredefinedRectangleFillStyle.PREDEFINED_STYLES[0],
+        true,
+        PredefinedStraightStrokeStyle.PREDEFINED_STYLES[0],
+        StraightStrokeDashPattern.SOLID,
+        false,
+    );
+
+    static defaultLineExtra: LineExtra = new LineExtra(
+        false,
+        PredefinedStraightStrokeStyle.PREDEFINED_STYLES[0],
+        false,
+        PredefinedAnchorChar.PREDEFINED_ANCHOR_CHARS[0],
+        false,
+        PredefinedAnchorChar.PREDEFINED_ANCHOR_CHARS[0],
+        StraightStrokeDashPattern.SOLID,
+        false,
+    );
+
+    static defaultTextAlign: TextAlign = new TextAlign(
+        TextHorizontalAlign.MIDDLE,
+        TextVerticalAlign.MIDDLE,
+    );
+
+    private static defaultExtraStateUpdateMutableFlow = new Flow<void>();
+    static defaultExtraStateUpdateFlow: Flow<void> = ShapeExtraManager.defaultExtraStateUpdateMutableFlow.immutable();
+
+    static setDefaultValues(
+        {
+            isFillEnabled,
+            fillStyleId,
+
+            isBorderEnabled,
+            borderStyleId,
+            isBorderRoundedCorner,
+
+            borderDashPattern,
+
+            isLineStrokeEnabled,
+            lineStrokeStyleId,
+            isLineStrokeRoundedCorner,
+
+            lineDashPattern,
+
+            isStartHeadAnchorCharEnabled,
+            startHeadAnchorCharId,
+
+            isEndHeadAnchorCharEnabled,
+            endHeadAnchorCharId,
+
+            textHorizontalAlign,
+            textVerticalAlign,
+        }: {
+            isFillEnabled?: boolean,
+            fillStyleId?: string,
+
+            isBorderEnabled?: boolean,
+            borderStyleId?: string,
+            isBorderRoundedCorner?: boolean,
+
+            borderDashPattern?: StraightStrokeDashPattern,
+
+            isLineStrokeEnabled?: boolean,
+            lineStrokeStyleId?: string,
+            isLineStrokeRoundedCorner?: boolean,
+
+            lineDashPattern?: StraightStrokeDashPattern,
+
+            isStartHeadAnchorCharEnabled?: boolean,
+            startHeadAnchorCharId?: string,
+
+            isEndHeadAnchorCharEnabled?: boolean,
+            endHeadAnchorCharId?: string,
+
+            textHorizontalAlign?: TextHorizontalAlign,
+            textVerticalAlign?: TextVerticalAlign,
+        }): void {
+        ShapeExtraManager.defaultRectangleExtra = RectangleExtra.create(
+            isFillEnabled ?? ShapeExtraManager.defaultRectangleExtra.isFillEnabled,
+            ShapeExtraManager.getRectangleFillStyle(fillStyleId),
+            isBorderEnabled ?? ShapeExtraManager.defaultRectangleExtra.isBorderEnabled,
+            ShapeExtraManager.getRectangleBorderStyle(borderStyleId),
+            borderDashPattern ?? ShapeExtraManager.defaultRectangleExtra.dashPattern,
+            isBorderRoundedCorner ?? ShapeExtraManager.defaultRectangleExtra.isRoundedCorner,
+        );
+
+        ShapeExtraManager.defaultLineExtra = new LineExtra(
+            isLineStrokeEnabled ?? ShapeExtraManager.defaultLineExtra.isStrokeEnabled,
+            ShapeExtraManager.getLineStrokeStyle(lineStrokeStyleId),
+            isStartHeadAnchorCharEnabled ?? ShapeExtraManager.defaultLineExtra.isStartAnchorEnabled,
+            ShapeExtraManager.getStartHeadAnchorChar(startHeadAnchorCharId),
+            isEndHeadAnchorCharEnabled ?? ShapeExtraManager.defaultLineExtra.isEndAnchorEnabled,
+            ShapeExtraManager.getEndHeadAnchorChar(endHeadAnchorCharId),
+            lineDashPattern ?? StraightStrokeDashPattern.SOLID,
+            isLineStrokeRoundedCorner ?? ShapeExtraManager.defaultLineExtra.isRoundedCorner,
+        );
+
+        ShapeExtraManager.defaultTextAlign = new TextAlign(
+            textHorizontalAlign ?? ShapeExtraManager.defaultTextAlign.horizontalAlign,
+            textVerticalAlign ?? ShapeExtraManager.defaultTextAlign.verticalAlign,
+        );
+
+        ShapeExtraManager.defaultExtraStateUpdateMutableFlow.value = undefined;
+    }
+
+    static getRectangleFillStyle(
+        id?: string,
+        defaultStyle: RectangleFillStyle = ShapeExtraManager.defaultRectangleExtra.userSelectedFillStyle,
+    ): RectangleFillStyle {
+        if (id === undefined) {
+            return defaultStyle;
+        }
+        return PredefinedRectangleFillStyle.PREDEFINED_STYLE_MAP[id] ?? defaultStyle;
+    }
+
+    static getAllPredefinedRectangleFillStyles(): RectangleFillStyle[] {
+        return PredefinedRectangleFillStyle.PREDEFINED_STYLES;
+    }
+
+    static getRectangleBorderStyle(
+        id?: string,
+        defaultStyle: StraightStrokeStyle = ShapeExtraManager.defaultRectangleExtra.userSelectedBorderStyle,
+    ): StraightStrokeStyle {
+        if (id === undefined) {
+            return defaultStyle;
+        }
+        return PredefinedStraightStrokeStyle.getStyle(id) ?? defaultStyle;
+    }
+
+    static getLineStrokeStyle(
+        id?: string,
+        defaultStyle: StraightStrokeStyle = ShapeExtraManager.defaultLineExtra.userSelectedStrokeStyle,
+    ): StraightStrokeStyle {
+        if (id === undefined) {
+            return defaultStyle;
+        }
+        return PredefinedStraightStrokeStyle.getStyle(id) ?? defaultStyle;
+    }
+
+    static getAllPredefinedStrokeStyles(): StraightStrokeStyle[] {
+        return PredefinedStraightStrokeStyle.PREDEFINED_STYLES;
+    }
+
+    static getStartHeadAnchorChar(
+        id?: string,
+        defaultChar: AnchorChar = ShapeExtraManager.defaultLineExtra.userSelectedStartAnchor,
+    ): AnchorChar {
+        if (id === undefined) {
+            return defaultChar;
+        }
+        return PredefinedAnchorChar.PREDEFINED_ANCHOR_CHAR_MAP[id] ?? defaultChar;
+    }
+
+    static getEndHeadAnchorChar(
+        id?: string,
+        defaultChar: AnchorChar = ShapeExtraManager.defaultLineExtra.userSelectedEndAnchor,
+    ): AnchorChar {
+        if (id === undefined) {
+            return defaultChar;
+        }
+        return PredefinedAnchorChar.PREDEFINED_ANCHOR_CHAR_MAP[id] ?? defaultChar;
+    }
+
+    static getAllPredefinedAnchorChars(): AnchorChar[] {
+        return PredefinedAnchorChar.PREDEFINED_ANCHOR_CHARS;
+    }
+}
