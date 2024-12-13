af5af582486efd8d9ea07b6cdcb61dcddafebf56
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-12-13 10:21:38 +0900

    Update application active state

--- monosketch-svelte/src/lib/mono/window/window-viewmodel.ts
+++ monosketch-svelte/src/lib/mono/window/window-viewmodel.ts
@@ -2,4 +2,6 @@ import { Flow } from '$libs/flow';
 
 export namespace WindowViewModel {
     export const windowSizeUpdateEventFlow = new Flow<boolean>(true);
+
+    export const applicationActiveStateFlow = new Flow<boolean>(true);
 }
