740f8423ab0dee5d95ab595f0b0246fcf7d3715c
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-12-14 07:25:26 +0900

    Use record instead of switch

--- monosketch-svelte/src/lib/mono/workspace/canvas/board-canvas-view-controller.ts
+++ monosketch-svelte/src/lib/mono/workspace/canvas/board-canvas-view-controller.ts
@@ -1,7 +1,7 @@
 import type { AppUiStateManager } from "$mono/ui-state-manager/app-ui-state-manager";
 import { BaseCanvasViewController } from '$mono/workspace/canvas/base-canvas-controller';
 import { HighlightType, type Pixel } from '$mono/monobitmap/board/pixel';
-import { ThemeColors } from '$mono/ui-state-manager/states';
+import { type ThemeColor, ThemeColors } from '$mono/ui-state-manager/states';
 import type { MonoBoard } from '$mono/monobitmap/board/board';
 
 /**
@@ -31,22 +31,15 @@ export class BoardCanvasViewController extends BaseCanvasViewController {
         if (pixel.isTransparent) {
             return;
         }
-        this.context.fillStyle = this.appUiStateManager.getThemedColorCode(this.getPixelColor(pixel));
+        const pixelColor = HighlightTypeToThemeColor[pixel.highlight];
+        this.context.fillStyle = this.appUiStateManager.getThemedColorCode(pixelColor);
         this.drawText(pixel.visualChar, row, column);
     };
+}
 
-    private getPixelColor = (pixel: Pixel) => {
-        switch (pixel.highlight) {
-            case HighlightType.NO:
-                return ThemeColors.Shape;
-            case HighlightType.SELECTED:
-                return ThemeColors.ShapeSelected;
-            case HighlightType.TEXT_EDITING:
-                return ThemeColors.ShapeTextEditing;
-            case HighlightType.LINE_CONNECT_FOCUSING:
-                return ThemeColors.ShapeLineConnectTarget;
-            default:
-                throw new Error(`Unknown highlight type: ${pixel.highlight}`);
-        }
-    };
+const HighlightTypeToThemeColor: Record<HighlightType, ThemeColor> = {
+    [HighlightType.NO]: ThemeColors.Shape,
+    [HighlightType.SELECTED]: ThemeColors.ShapeSelected,
+    [HighlightType.TEXT_EDITING]: ThemeColors.ShapeTextEditing,
+    [HighlightType.LINE_CONNECT_FOCUSING]: ThemeColors.ShapeLineConnectTarget,
 }
