2bc940bbdf6781389b1926de7d322c8149135489
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-12-17 17:01:51 +0900

    Line's heads

--- monosketch-svelte/src/lib/ui/pannel/shapetool/appearance/EndHeadTool.svelte
+++ monosketch-svelte/src/lib/ui/pannel/shapetool/appearance/EndHeadTool.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
-import CommonLineAnchorTool from './common/CommonLineAnchorTool.svelte';
+    import CommonLineAnchorTool from './common/CommonLineAnchorTool.svelte';
+    import type { ShapeToolViewModel } from "$ui/pannel/shapetool/viewmodel/shape-tool-viewmodel";
+    import { getContext, onDestroy, onMount } from "svelte";
+    import { SHAPE_TOOL_VIEWMODEL } from "$ui/pannel/shapetool/constants";
+    import { OneTimeAction } from "$mono/action-manager/one-time-actions";
+    import { LifecycleOwner } from "$libs/flow";
 
-let selectedId: string = 'A0';
+    let viewModel: ShapeToolViewModel = getContext(SHAPE_TOOL_VIEWMODEL);
+    let lifecycleOwner = new LifecycleOwner();
+
+    let anchorItem = viewModel.lineEndHeadFlow.value;
+
+    function onItemSelect(id: string | null) {
+        viewModel.update(OneTimeAction.ChangeLineEndAnchorExtra({ newHeadId: id, isEnabled: id !== null }));
+    }
+
+    onMount(() => {
+        lifecycleOwner.onStart();
+
+        viewModel.lineEndHeadFlow.observe(lifecycleOwner, (value) => {
+            anchorItem = value;
+        });
+    });
+
+    onDestroy(() => {
+        lifecycleOwner.onStop();
+    });
 </script>
-
-<CommonLineAnchorTool title="End head" bind:selectedId />
+{#if anchorItem}
+    <CommonLineAnchorTool title="End head" selectedId="{anchorItem.selectedId}" {onItemSelect}/>
+{/if}
--- monosketch-svelte/src/lib/ui/pannel/shapetool/appearance/StartHeadTool.svelte
+++ monosketch-svelte/src/lib/ui/pannel/shapetool/appearance/StartHeadTool.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
-import CommonLineAnchorTool from './common/CommonLineAnchorTool.svelte';
+    import CommonLineAnchorTool from './common/CommonLineAnchorTool.svelte';
+    import type { ShapeToolViewModel } from "$ui/pannel/shapetool/viewmodel/shape-tool-viewmodel";
+    import { getContext, onDestroy, onMount } from "svelte";
+    import { SHAPE_TOOL_VIEWMODEL } from "$ui/pannel/shapetool/constants";
+    import { OneTimeAction } from "$mono/action-manager/one-time-actions";
+    import { LifecycleOwner } from "$libs/flow";
 
-let selectedId: string = 'A0';
+    let viewModel: ShapeToolViewModel = getContext(SHAPE_TOOL_VIEWMODEL);
+    let lifecycleOwner = new LifecycleOwner();
+
+    let anchorItem = viewModel.lineStartHeadFlow.value;
+
+    function onItemSelect(id: string | null) {
+        viewModel.update(OneTimeAction.ChangeLineStartAnchorExtra({ newHeadId: id, isEnabled: id !== null }));
+    }
+
+    onMount(() => {
+        lifecycleOwner.onStart();
+
+        viewModel.lineStartHeadFlow.observe(lifecycleOwner, (value) => {
+            anchorItem = value;
+        });
+    });
+
+    onDestroy(() => {
+        lifecycleOwner.onStop();
+    });
 </script>
 
-<CommonLineAnchorTool title="Start head" bind:selectedId />
+{#if anchorItem}
+    <CommonLineAnchorTool title="Start head" selectedId="{anchorItem.selectedId}" {onItemSelect}/>
+{/if}
