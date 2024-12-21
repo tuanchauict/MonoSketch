39e4bb14de779da68af85972c6c75f6764d69131
Tuan Chau (Tuna) <tuan@iamtuna.org>
2024-12-21 12:11:58 +0900

    Fix UUID

--- monosketch-svelte/src/lib/mono/uuid/index.ts
+++ monosketch-svelte/src/lib/mono/uuid/index.ts
@@ -1,10 +1,38 @@
+/**
+ * An object class that generate unique ID.
+ * ### Current version
+ * Version 2:
+ * ```
+ * Structure: version datetime random random
+ * Length   :   3        8      10     10
+ * ```
+ * Version: 02-
+ * Datetime: Base64-like encoded current time (8 chars)
+ * Random: Base64-like encoded random number (10 chars)
+ * Characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+ *
+ * ### Older versions:
+ * Version 1:
+ * ```
+ * Structure: version datetime random random
+ * Length   :   3        8      10     10
+ * ```
+ * Version: 01-
+ * Datetime: Base64-like encoded current time (8 chars)
+ * Random: Base64-like encoded random number (10 chars)
+ * Characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+="
+ */
 export class UUID {
     private static readonly VERSION = '02';
     private static readonly BASE64_CHARS =
         'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'.split('');
 
+    private static now(): number {
+        return Date.now();
+    }
+
     static generate(): string {
-        const part1 = new Date().getTime().toString(2).slice(0, -3);
+        const part1 = this.toBase64(this.now()).slice(0, -2);
         const part2 = UUID.toBase64(UUID.getRandomLong());
         const part3 = UUID.toBase64(UUID.getRandomLong());
         return `${UUID.VERSION}-${part1}${part2}${part3}`;
