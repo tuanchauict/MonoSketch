76f94ae7e0e41d0352ed4d6dcfca6d6f2f19387d
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-02-02 07:06:11 +0900

    Add Portal to allow modal to be appended outside the host component

---
+++ monosketch-svelte/src/lib/ui/modal/common/Portal.svelte
@@ -0,0 +1,21 @@
+<!--
+  - Copyright (c) 2025, tuanchauict
+  -->
+
+<script lang="ts">
+    export let parent: HTMLElement = document.querySelector('#modal')!!;
+
+    function portal(node: HTMLElement) {
+        parent.appendChild(node);
+
+        return {
+            destroy() {
+                node.remove();
+            },
+        };
+    }
+</script>
+
+<div use:portal class="portal">
+    <slot/>
+</div>
