b8c7752d55cd1a94e578000f102926ff2b66202c
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-02-02 13:09:58 +0900

    Wrap Project manager into a component

---
+++ monosketch-svelte/src/lib/ui/nav/project/ProjectManager.svelte
@@ -0,0 +1,14 @@
+<!--
+  - Copyright (c) 2025, tuanchauict
+  -->
+
+<script>
+import WorkingProject from "$ui/nav/project/WorkingProject.svelte";
+import ProjectManagerButton from "$ui/nav/project/ProjectManagerButton.svelte";
+import ToolbarContainer from "$ui/nav/common/ToolbarContainer.svelte";
+</script>
+
+<ToolbarContainer>
+    <ProjectManagerButton />
+    <WorkingProject />
+</ToolbarContainer>
