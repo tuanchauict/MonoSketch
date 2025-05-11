e3d736923336227d080d625db6951948be6634f8
Tuan Chau (Tuna) <tuan@iamtuna.org>
2025-05-11 12:40:28 +0900

    Add title and content components

---
+++ monosketch-svelte/src/lib/ui/modal/export/Title.svelte
@@ -0,0 +1,16 @@
+<!--
+  - Copyright (c) 2025, tuanchauict
+  -->
+
+<span>Export</span>
+
+<style lang="scss">
+    span {
+        font-size: 18px;
+        font-weight: bold;
+        margin-bottom: 16px;
+        text-align: center;
+        display: block;
+        width: 100%;
+    }
+</style>
